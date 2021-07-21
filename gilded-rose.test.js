ed4176578c0a8730761cbfe6527a768b165af15a
Mike Abney <mike@kajabi.com>
2021-07-21 11:28:26 -0500

    Clean up tests slightly with new matcher

--- gilded-rose.test.js
+++ gilded-rose.test.js
@@ -1,209 +1,191 @@
 const tick = require("./");
 
-describe ("Gilded Rose", () => {
-  function tickAndCheck(input, expected) {
-    expect(tick(input)).toEqual(expected);
-  };
+expect.extend({
+  toMatchSellInAndQuality(received, sellIn, quality) {
+    const recievedValues = `[sellIn: ${received.sellIn}, quality: ${received.quality}]`;
+    const expectedValues = `[sellIn: ${sellIn}, quality: ${quality}]`;
+    const valuesComparison = `\n  Received ${recievedValues}\n  Expected ${expectedValues}`;
+    if (received.sellIn == sellIn && received.quality == quality) {
+      return {
+        pass: true,
+        message: () => `expected values not to match:${valuesComparison}`
+      }
+    }
+    return {
+      pass: false,
+      message: () => `expected values to match:${valuesComparison}`
+    }
+  },
+});
 
+describe("Gilded Rose", () => {
   describe("Normal Item", () => {
     test("before sell date", () => {
-      input = [{name: "Normal Item", sellIn: 5, quality: 10}];
-      expected = [{name: "Normal Item", sellIn: 4, quality: 9}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Normal Item", sellIn: 5, quality: 10 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(4, 9);
     });
 
     test("on sell date", () => {
-      input = [{name: "Normal Item", sellIn: 0, quality: 10}];
-      expected = [{name: "Normal Item", sellIn: -1, quality: 8}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Normal Item", sellIn: 0, quality: 10 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(-1, 8);
     });
 
     test("after sell date", () => {
-      input = [{name: "Normal Item", sellIn: -10, quality: 10}];
-      expected = [{name: "Normal Item", sellIn: -11, quality: 8}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Normal Item", sellIn: -10, quality: 10 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(-11, 8);
     });
 
     test("of zero quality", () => {
-      input = [{name: "Normal Item", sellIn: 5, quality: 0}];
-      expected = [{name: "Normal Item", sellIn: 4, quality: 0}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Normal Item", sellIn: 5, quality: 0 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(4, 0);
     });
   });
 
   describe("Aged Brie", () => {
     test("before sell date", () => {
-      input = [{name: "Aged Brie", sellIn: 5, quality: 10}];
-      expected = [{name: "Aged Brie", sellIn: 4, quality: 11}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Aged Brie", sellIn: 5, quality: 10 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(4, 11);
     });
 
     test("with max quality", () => {
-      input = [{name: "Aged Brie", sellIn: 5, quality: 50}];
-      expected = [{name: "Aged Brie", sellIn: 4, quality: 50}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Aged Brie", sellIn: 5, quality: 50 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(4, 50);
     });
 
     test("on sell date", () => {
-      input = [{name: "Aged Brie", sellIn: 0, quality: 10}];
-      expected = [{name: "Aged Brie", sellIn: -1, quality: 12}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Aged Brie", sellIn: 0, quality: 10 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(-1, 12);
     });
 
     test("on sell date near max quality", () => {
-      input = [{name: "Aged Brie", sellIn: 0, quality: 49}];
-      expected = [{name: "Aged Brie", sellIn: -1, quality: 50}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Aged Brie", sellIn: 0, quality: 49 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(-1, 50);
     });
 
     test("on sell date with max quality", () => {
-      input = [{name: "Aged Brie", sellIn: 0, quality: 50}];
-      expected = [{name: "Aged Brie", sellIn: -1, quality: 50}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Aged Brie", sellIn: 0, quality: 50 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(-1, 50);
     });
 
     test("after sell date", () => {
-      input = [{name: "Aged Brie", sellIn: -10, quality: 10}];
-      expected = [{name: "Aged Brie", sellIn: -11, quality: 12}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Aged Brie", sellIn: -10, quality: 10 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(-11, 12);
     });
 
     test("after sell date with max quality", () => {
-      input = [{name: "Aged Brie", sellIn: -10, quality: 50}];
-      expected = [{name: "Aged Brie", sellIn: -11, quality: 50}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Aged Brie", sellIn: -10, quality: 50 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(-11, 50);
     });
   });
 
   describe("Legendary Item (Sulfuras, Hand of Ragnaros)", () => {
     test("before sell date", () => {
-      input = [{name: "Sulfuras, Hand of Ragnaros", sellIn: 5, quality: 80}];
-      expected = [{name: "Sulfuras, Hand of Ragnaros", sellIn: 5, quality: 80}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Sulfuras, Hand of Ragnaros", sellIn: 5, quality: 80 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(5, 80);
     });
 
     test("on sell date", () => {
-      input = [{name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80}];
-      expected = [{name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(0, 80);
     });
 
     test("after sell date", () => {
-      input = [{name: "Sulfuras, Hand of Ragnaros", sellIn: -10, quality: 80}];
-      expected = [{name: "Sulfuras, Hand of Ragnaros", sellIn: -10, quality: 80}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Sulfuras, Hand of Ragnaros", sellIn: -10, quality: 80 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(-10, 80);
     });
   });
 
   describe("Backstage Pass", () => {
     test("long before sell date", () => {
-      input = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 11, quality: 10}];
-      expected = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 11}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 11, quality: 10 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(10, 11);
     });
 
     test("long before sell date at max quality", () => {
-      input = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 11, quality: 50}];
-      expected = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 50}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 11, quality: 50 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(10, 50);
     });
 
     test("medium close to sell date upper bound", () => {
-      input = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 10}];
-      expected = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 9, quality: 12}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 10 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(9, 12);
     });
 
     test("medium close to sell date upper bound at max quality", () => {
-      input = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 50}];
-      expected = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 9, quality: 50}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 50 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(9, 50);
     });
 
     test("medium close to sell date lower bound", () => {
-      input = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 6, quality: 10}];
-      expected = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 12}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 6, quality: 10 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(5, 12);
     });
 
     test("medium close to sell date lower bound at max quality", () => {
-      input = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 6, quality: 50}];
-      expected = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 50}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 6, quality: 50 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(5, 50);
     });
 
     test("very close to sell date upper bound", () => {
-      input = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 10}];
-      expected = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 4, quality: 13}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 10 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(4, 13);
     });
 
     test("very close to sell date upper bound at max quality", () => {
-      input = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 50}];
-      expected = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 4, quality: 50}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 50 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(4, 50);
     });
 
     test("very close to sell date lower bound", () => {
-      input = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 1, quality: 10}];
-      expected = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 0, quality: 13}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 1, quality: 10 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(0, 13);
     });
 
     test("very close to sell date lower bound at max quality", () => {
-      input = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 1, quality: 50}];
-      expected = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 0, quality: 50}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 1, quality: 50 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(0, 50);
     });
 
     test("on sell date", () => {
-      input = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 0, quality: 10}];
-      expected = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: -1, quality: 0}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 0, quality: 10 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(-1, 0);
     });
 
     test("after sell date", () => {
-      input = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: -10, quality: 10}];
-      expected = [{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: -11, quality: 0}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Backstage passes to a TAFKAL80ETC concert", sellIn: -10, quality: 10 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(-11, 0);
     });
   });
 
   describe.skip("Conjured Item", () => {
     test("before sell date", () => {
-      input = [{name: "Conjured Mana Cake", sellIn: 5, quality: 10}];
-      expected = [{name: "Conjured Mana Cake", sellIn: 4, quality: 8}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Conjured Mana Cake", sellIn: 5, quality: 10 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(4, 8);
     });
 
     test("before sell date at zero quality", () => {
-      input = [{name: "Conjured Mana Cake", sellIn: 5, quality: 0}];
-      expected = [{name: "Conjured Mana Cake", sellIn: 4, quality: 0}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Conjured Mana Cake", sellIn: 5, quality: 0 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(4, 0);
     });
 
     test("on sell date", () => {
-      input = [{name: "Conjured Mana Cake", sellIn: 0, quality: 10}];
-      expected = [{name: "Conjured Mana Cake", sellIn: -1, quality: 6}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Conjured Mana Cake", sellIn: 0, quality: 10 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(-1, 6);
     });
 
     test("on sell date at zero quality", () => {
-      input = [{name: "Conjured Mana Cake", sellIn: 0, quality: 0}];
-      expected = [{name: "Conjured Mana Cake", sellIn: -1, quality: 0}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Conjured Mana Cake", sellIn: 0, quality: 0 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(-1, 0);
     });
 
     test("after sell date", () => {
-      input = [{name: "Conjured Mana Cake", sellIn: -10, quality: 10}];
-      expected = [{name: "Conjured Mana Cake", sellIn: -11, quality: 6}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Conjured Mana Cake", sellIn: -10, quality: 10 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(-11, 6);
     });
 
     test("after sell date at zero quality", () => {
-      input = [{name: "Conjured Mana Cake", sellIn: -10, quality: 0}];
-      expected = [{name: "Conjured Mana Cake", sellIn: -11, quality: 0}];
-      tickAndCheck(input, expected);
+      input = [{ name: "Conjured Mana Cake", sellIn: -10, quality: 0 }];
+      expect(tick(input)[0]).toMatchSellInAndQuality(-11, 0);
     });
   });
 });
